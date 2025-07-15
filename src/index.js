dd798e0c0c62f5345f70e5be897969d4f64eedf5
Marshall Chan <yahoohung@gmail.com>
2025-07-15 06:24:07 +0000

    again

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
+import "./wdyr";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import React from "react";
 import App from "./App";
-import WhyDidYouRender from "@welldone-software/why-did-you-render";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-
-WhyDidYouRender(React, { trackAllPureComponents: true });
 
 root.render(
   <StrictMode>
